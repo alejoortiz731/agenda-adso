a7365b670b5a935795e209e51996d17f80195e6f
alejoortiz731 <ortizdanielalejandro789@gmail.com>
2026-07-22 16:30:37 -0500

    Mi primer clase con ReactJs

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+const fecha = new Date().toLocaleString();
+return (
+  <main>
+      <h1>Hola SENA</h1>
+      <p>Soy Daniel Alejandro Gonzalez Ortiz, Esta es mi primera clase de ReactJs</p>
+  </main>
+)
+}
