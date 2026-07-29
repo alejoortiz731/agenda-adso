c19eaaa99254bd7087023b7f86503fd3eed0606c
alejoortiz731 <ortizdanielalejandro789@gmail.com>
2026-07-29 15:51:51 -0500

    Agregar ejercicio de JavaScript ES6+

---
+++ src/ejercicio.js
@@ -0,0 +1,14 @@
+const nombre = "Daniel";
+const ficha = 3412768;
+const notas = [2.0, 2.5, 2.8];
+const promedio = (notas[0] + notas[1] + notas[2]) / 3;
+
+console.log("======================================");
+console.log("       SISTEMA DE NOTAS SENA");
+console.log("======================================");
+console.log(`Aprendiz: ${nombre}`);
+console.log(`Ficha: ${ficha}`);
+console.log(`Notas: ${notas}`);
+console.log("======================================");
+console.log(`Promedio: ${promedio.toFixed(2)}`);
+console.log(`Estado: ${promedio >= 3 ? "Aprobado" : "No Aprobado"}`);
---
+++ src/ejercicio_clase_2.js
@@ -0,0 +1,13 @@
+const nombre = "Daniel";
+const ficha = 3412768
+const notas = [4.0, 4.5, 3.8];
+const promedio = (notas[0] + notas[1] + notas[2]) / 3;
+console.log(`======================================`)
+console.log(`       SISTEMA DE NOTAS SENA`)
+console.log(`======================================`)
+console.log(`Aprendiz: ${nombre}`);
+console.log(`Ficha: ${ficha}`);
+console.log(`Notas: ${notas}`)
+console.log(`======================================`)
+console.log(`Promedio: ${promedio.toFixed(2)}`);
+console.log(`Estado: ${promedio >= 3 ? "Aprobado" : "No Aprobado"}`);
