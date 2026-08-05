fb7d1c56b5c5e629c4945b15ecc8c662175ad520
Alejandro_011 <ortizdanielalejandro789@gmail.com>
2026-08-05 16:29:49 -0500

    Primer version de agenda-adso

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,35 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react' 
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// React: la librería principal 
+
+ 
+
+import ReactDOM from 'react-dom/client' 
+
+// ReactDOM: dibuja React en el navegador 
+
+ 
+
+import App from './App.jsx' 
+
+// Importamos el componente raíz (nuestra Agenda ADSO) 
+
+ 
+
+import './index.css' 
+
+// Importamos los estilos globales 
+
+ 
+
+ReactDOM.createRoot(document.getElementById('root')).render( 
+
+  <React.StrictMode> 
+
+    <App /> 
+
+    {/* Aquí montamos nuestra Agenda ADSO */} 
+
+  </React.StrictMode>, 
+
+); 
