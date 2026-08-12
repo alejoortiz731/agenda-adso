bbf3a7d10dedfe1621a9b5fcfc9771fb36325f98
alejoortiz731 <ortizdanielalejandro789@gmail.com>
2026-08-12 15:44:36 -0500

    Clase_4_Agenda_ADSO_v2

--- src/App.jsx
+++ src/App.jsx
@@ -1,116 +1,43 @@
-import "./App.css"; // Importamos estilos de la app 
 
-import ContactoCard from "./components/ContactoCard"; // Importamos el componente hijo 
+import { useState } from "react";
+import "./App.css";
+import ContactoCard from "./components/ContactoCard";
+import FormularioContacto from "./components/FormularioContacto";
 
- 
+export default function App() {
+  const [contactos, setContactos] = useState([
 
-export default function App() { 
+  ]);
 
-  // Esta es nuestra "base de datos" inicial quemada en el código 
+  // Agregar
+  const agregarContacto = (nuevo) => {
+    setContactos((prev) => [...prev, { id: Date.now(), ...nuevo }]);
+  };
 
-  const contactos = [ 
+  // Eliminar
+  const eliminarContacto = (id) => {
+    setContactos((prev) => prev.filter((c) => c.id !== id));
+  };
 
-    { 
+  return (
+    <main className="app-container">
+      <h1 className="app-title">Agenda ADSO v2</h1>
 
-      id: 1, 
+      <FormularioContacto onAgregar={agregarContacto} />
 
-      nombre: "Carolina Pérez", 
-
-      telefono: "300 123 4567", 
-
-      correo: "carolina@sena.edu.co", 
-
-      etiqueta: "Compañera", 
-
-    }, 
-
-    { 
-
-      id: 2, 
-
-      nombre: "Juan Díaz", 
-
-      telefono: "301 987 6543", 
-
-      correo: "juan@sena.edu.co", 
-
-      etiqueta: "Instructor", 
-
-    }, 
-
-    { 
-
-      id: 3, 
-
-      nombre: "Luisa Martínez", 
-
-      telefono: "320 555 7788", 
-
-      correo: "luisa@sena.edu.co", 
-
-      etiqueta: "Cliente", 
-
-    }, 
-    { 
-
-      id: 4, 
-
-      nombre: "Luisa", 
-
-      telefono: "320 555 7788", 
-
-      correo: "luisa@sena.edu.co", 
-
-      etiqueta: "Cliente", 
-
-    }, 
-
-  ]; 
-
- 
-
-  return ( 
-
-    <main className="app-container"> 
-
-      <h1 className="app-title">Agenda ADSO 📒</h1> 
-
- 
-
-      <p className="app-subtitle">Contactos guardados</p> 
-
- 
-
-      {/* Recorremos el arreglo contactos y pintamos una tarjeta por cada uno */} 
-
-      {contactos.map((c) => ( 
-
-        <ContactoCard 
-
-          key={c.id}            // key única para React 
-
-          nombre={c.nombre}     // prop nombre 
-
-          telefono={c.telefono} // prop telefono 
-
-          correo={c.correo}     // prop correo 
-
-          etiqueta={c.etiqueta} // prop etiqueta (Cliente, Instructor, etc.) 
-
-        /> 
-
-      ))} 
-
- 
-
-      <p className="app-nota"> 
-
-        (Versión 0.1 - solo lectura, sin agregar ni editar todavía) 
-
-      </p> 
-
-    </main> 
-
-  ); 
-
+      <section className="lista-contactos">
+        {contactos.map((c) => (
+          <ContactoCard
+            key={c.id}
+            id={c.id}
+            nombre={c.nombre}
+            telefono={c.telefono}
+            correo={c.correo}
+            etiqueta={c.etiqueta}
+            onDelete={eliminarContacto}
+          />
+        ))}
+      </section>
+    </main>
+  );
 }
